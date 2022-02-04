f7a6a1a649aeb2e171d25f970b392ea42e6557cd
Shao-Yu, Chu <b06705028@ntu.edu.tw>
2022-02-04 19:39:32 +0800

    refactor

--- solver.py
+++ solver.py
@@ -1,110 +1,143 @@
 from pathlib import Path
+from copy import deepcopy
+from typing import List
+from os import cpu_count
 
 from scipy.stats import entropy
 
-from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+from multiprocessing import Pool
 
 WORD_LEN = 5
-MATCHES = []
-for a in range(WORD_LEN + 1):
-    for b in range(WORD_LEN + 1 - a):
-        MATCHES.append(f"{a}a{b}b")
 
 
-def get_valid_words(valid_words):
-    """Filter words and return those containing 5 unique characters"""
+class WordleHelper:
+    def __init__(self, word_list: List[str]):
+        self.all_words = word_list
+        self.valid_words = deepcopy(word_list)
+        self.last_guess = None
+        self.match_results = []
+        for a in range(WORD_LEN + 1):
+            for b in range(WORD_LEN + 1 - a):
+                self.match_results.append(f"{a}a{b}b")
+        self.match_results.remove(f"{WORD_LEN-1}a1b")
+
+    def _match(self, word: str, guess: str):
+        """?a?b
+
+        "a" is the number of matched characters in the correct position,
+        "b" is the number of matched characters in a wrong position
+        """
+        a, b = 0, 0
+        matched_char = set()
+        for i in range(WORD_LEN):
+            if word[i] == guess[i]:
+                a += 1
+                matched_char.add(word[i])
+        b = len(set(word).intersection(set(guess)).difference(matched_char))
+        return f"{a}a{b}b"
+
+    def _eval_guess(self, guess: str):
+        """Evaluate a guess and return the resulting entropy."""
+        match_dist = {mat: 0 for mat in self.match_results}
+        # compare to the word list
+        for word in self.valid_words:
+            match_dist[self._match(word, guess)] += 1
+
+        # compute entropy
+        total = sum(list(match_dist.values()))
+        probs = [val / total for val in match_dist.values()]
+        return entropy(probs)
+
+    def get_best_guess(self, top_k: int = 3, progress_bar: bool = True):
+        """Get the top `top_k` guesses.
+
+        If there are less than `top_k` valid words, return all."""
+        # evaluate all guesses
+        if len(self.valid_words) < len(self.all_words) * 0.01:
+            search_list = self.valid_words
+        else:
+            search_list = self.all_words
+        if progress_bar:
+            entropies = process_map(
+                self._eval_guess,
+                search_list,
+                max_workers=cpu_count(),
+                chunksize=cpu_count(),
+            )
+        else:
+            with Pool(processes=cpu_count()) as pool:
+                entropies = pool.map(self._eval_guess, search_list)
+
+        # sort based on entropy
+        all_gue_ent = sorted(
+            list(zip(search_list, entropies)), key=lambda x: x[1], reverse=True
+        )
+        n_best = min(top_k, len(all_gue_ent))
+        return [gue_ent[0] for gue_ent in all_gue_ent[:n_best]]
+
+    def update_valid_words(self, guess: str, a_chars: str, b_chars: str):
+        """Update the list of valid words based on the guess and its results.
+
+        Parameters
+        ----------
+        guess: str
+            The word you guessed.
+        a_chars: str
+            A string containing characters that appears to be green.
+        b_chars: str
+            A string containing characters that appears to be yellow.
+        """
+        guess = guess.lower()
+        a_chars, b_chars = a_chars.lower(), b_chars.lower()
+        new_valid_words = []
+        absent_chars = set(guess).difference(set(a_chars + b_chars))
+        for word in self.valid_words:
+            try:
+                for a_c in a_chars:
+                    assert word.index(a_c) == guess.index(a_c)
+                for b_c in b_chars:
+                    assert word.index(b_c) != guess.index(b_c)
+                for abs_c in absent_chars:
+                    assert abs_c not in word
+                new_valid_words.append(word)
+            except Exception:
+                pass
+        self.valid_words = new_valid_words
+
+
+def get_valid_words(word_list):
+    """Filter words and return those containing `WORD_LEN` unique characters"""
     return [
         word
-        for word in valid_words
+        for word in word_list
         if len(word) == WORD_LEN and len(set(word)) == WORD_LEN
     ]
 
 
-def match(word, guess):
-    """?a?b
-
-    "a" is the number of matched characters in the correct position,
-    "b" is the number of matched characters in a wrong position
-    """
-    a, b = 0, 0
-    matched_char = set()
-    for i in range(WORD_LEN):
-        if word[i] == guess[i]:
-            a += 1
-            matched_char.add(word[i])
-    b = len(set(word).intersection(set(guess)).difference(matched_char))
-    return f"{a}a{b}b"
-
-
-def update_word_list(valid_words, guess, a_chars, b_chars):
-    result = []
-    absent_chars = set(guess) - set(a_chars + b_chars)
-    for word in valid_words:
-        try:
-            for a_c in a_chars:
-                assert word.index(a_c) == guess.index(a_c)
-            for b_c in b_chars:
-                assert word.index(b_c) != guess.index(b_c)
-            for abs_c in absent_chars:
-                assert abs_c not in word
-            result.append(word)
-        except Exception:
-            pass
-    return result
-
-
-def eval_guess(guess, valid_words):
-    match_dist = {mat: 0 for mat in MATCHES}
-
-    # compare to the word list
-    for word in valid_words:
-        match_dist[match(word, guess)] += 1
-
-    # compute entropy
-    total = sum(list(match_dist.values()))
-    probs = [val / total for val in match_dist.values()]
-    return entropy(probs)
-
-
-def get_best_guess(valid_words, search_space, top: int = 3):
-    guess_entropy_pairs = []
-    for guess in tqdm(search_space):
-        ent = eval_guess(guess, valid_words)
-        guess_entropy_pairs.append((guess, ent))
-    guess_entropy_pairs = sorted(
-        guess_entropy_pairs, key=lambda x: x[1], reverse=True
-    )
-    return [
-        guess_entropy_pair[0]
-        for guess_entropy_pair in guess_entropy_pairs[
-            : min(top, len(guess_entropy_pairs))
-        ]
-    ]
-
-
 if __name__ == "__main__":
+
     # construct word list
     word_list_path = Path("data") / "words_alpha.txt"
     with open(word_list_path, "r") as fp:
-        valid_words = get_valid_words([word.rstrip() for word in fp])
-    all_words = [word for word in valid_words]
+        word_list = get_valid_words([word.rstrip() for word in fp])
 
+    # start wordle-solver
+    solver = WordleHelper(word_list)
     while True:
         # guess a word
-        if len(valid_words) > len(all_words) * 0.01:
-            best_guess = get_best_guess(valid_words, all_words)
-        else:
-            best_guess = get_best_guess(valid_words, valid_words)
+        best_guesses = solver.get_best_guess()
         print(
-            f"\nThe best guesses are '{best_guess}'"
-            f"(over {len(valid_words)}/{len(all_words)})."
+            f"\nThe suggested words are {best_guesses} "
+            f"(over {len(solver.valid_words)})."
         )
 
         # enter the guessing result
-        guess = input("The actual guess: ")
-        a_chars = input("The green characters: ")
-        b_chars = input("The yellow characters: ")
-        valid_words = update_word_list(valid_words, guess, a_chars, b_chars)
-        print(valid_words)
+        guess = input("Enter the word you guessed: ")
+        a_chars = input("Enter the green character(s): ")
+        b_chars = input("Enter the yellow character(s): ")
+        solver.update_valid_words(guess, a_chars, b_chars)
+
+        # hit
         if len(a_chars) == 5:
             break
